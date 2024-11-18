c316f757b977394d47db457dc35c0e659acef6a4
Clement Nathanael Lim <18222032@mahasiswa.itb.ac.id>
2024-11-18 14:36:13 +0700

    Update log-in.js

--- frontend/log-in.js
+++ frontend/log-in.js
@@ -34,9 +34,8 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
             }, 3000);
         } else {
             // Show error message if username or password is incorrect
-            const errorData = await response.json();
             const errorMessage = document.getElementById('errorMessage');
-            errorMessage.textContent = errorData.message || 'Username / password salah!';
+            errorMessage.textContent = result.message || 'Username / password salah!';
             errorMessage.style.display = 'block';
 
             // Hide the error message after 3 seconds
